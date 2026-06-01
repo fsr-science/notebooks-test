3b238e2e6a83b09aea1b161654ef22bd35490109
Harshit Saha <hsxtheemperor@gmail.com>
2026-06-02 00:25:29 +0530

    Initial Commit

--- zip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Zip everything except PDFs inside the SCIENCE directory
-zip -r my_archive.zip . -x "SCIENCE/NCERT-EXAMPLER/*" "SCIENCE/NCERT-PDF/*" "SCIENCE/REFERENCE-BOOKS-SOURCE/*" ".git/*" "zip.sh" "node_modules/*" ".venv/*" "__pycache__/*"
